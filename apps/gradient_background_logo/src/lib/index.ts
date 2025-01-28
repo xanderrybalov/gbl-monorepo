1f1367b8f8cbfc2f98a4d5e7f0a7c29e1049a0a1
Oleksandr Rybalov <xanderrybalov@gmail.com>
2025-01-28 17:38:07 +0200

    feat: added gbl app

--- apps/gradient_background_logo/src/lib/index.ts
+++ apps/gradient_background_logo/src/lib/index.ts
@@ -1,1 +1,2 @@
-// Reexport your entry components here
+export { default as BackgroundContainer } from './BackgroundContainer.svelte';
+export { default as Logo } from './Logo.svelte';
--- apps/gradient_background_logo/src/routes/+page.svelte
+++ apps/gradient_background_logo/src/routes/+page.svelte
@@ -1,3 +1,10 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { BackgroundContainer, Logo } from "$lib";
+
+
+      </script>
+      
+      <BackgroundContainer>
+        <Logo logoSrc="/bing-logo.svg" logoAlt="Bing Logo" caption="Bing" />
+      </BackgroundContainer>
+      
