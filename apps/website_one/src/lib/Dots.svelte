11655ba6e3550acefd5a48c082e31654adc84598
Oleksandr Rybalov <xanderrybalov@gmail.com>
2025-02-02 22:54:40 +0200

    fixed: dots component

--- apps/website_one/src/lib/Dots.svelte
+++ apps/website_one/src/lib/Dots.svelte
@@ -1,22 +1,23 @@
 <script lang="ts">
-  import type { CollectionSlide } from "./index.js";
+	import type { CollectionSlide } from './index.js';
 
-    export let currentIndex: number;
-    export let slides: CollectionSlide[];
-    export let setSlide: (index: number) => void;
-  </script>
-  
-  <div class="flex lg:gap-[2rem] xl:gap-[5rem] gap-[2rem]">
-    {#each slides, index}
-      <button
-        class="w-[0.5rem] h-[0.5rem] rounded-full transition-colors duration-300 focus:outline-none"
-        class:bg-[#e3ddd6]={currentIndex === index}
-        class:bg-[#6b6d73]={currentIndex !== index}
-        role="tab"
-        aria-selected={currentIndex === index}
-        aria-label={`Go to slide ${index + 1}`}
-        on:click={() => setSlide(index)}
-      ></button>
-    {/each}
-  </div>
-  
+	let { currentIndex, slides, setSlide } = $props<{
+		currentIndex: number;
+		slides: CollectionSlide[];
+		setSlide: (index: number) => void;
+	}>();
+</script>
+
+<div class="flex gap-[2rem] lg:gap-[2rem] xl:gap-[5rem]">
+	{#each slides, index}
+		<button
+			class="h-[0.5rem] w-[0.5rem] rounded-full transition-colors duration-300 focus:outline-none"
+			class:bg-[#e3ddd6]={currentIndex === index}
+			class:bg-[#6b6d73]={currentIndex !== index}
+			role="tab"
+			aria-selected={currentIndex === index}
+			aria-label={`Go to slide ${index + 1}`}
+			onclick={() => setSlide(index)}
+		></button>
+	{/each}
+</div>
